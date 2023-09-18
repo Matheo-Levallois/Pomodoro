c90d339b459d859fe9369442a5479d072ca2cdb7
Matheo Levallois <levallo221@campus.unicaen.fr>
2023-09-18 11:15:42 +0200

    implementation du timer

--- js.js
+++ js.js
@@ -5,9 +5,13 @@ const timerElement = document.getElementById("timer")
 
 
 
-function repos(){
-    const tempsRepos = 0.2;
-    let temps = tempsRepos * 60
+function lancer(){
+    
+    let temps = 0.2 * 60
+    let tempsTravail = true
+    travail.style.color = 'green';
+  pause.style.color = 'red';
+
 
   setInterval(() => {
    
@@ -20,9 +24,21 @@ function repos(){
     timerElement.innerText = `${minutes}:${secondes}`
     temps = temps <= 0 ? 0 : temps - 1
 
-    if(minutes == 0 && secondes == 0){
-      console.log("finish repos ");
-      commencer();
+    if (temps <= 0 && tempsTravail) {
+      tempsTravail = false;
+      temps = 0.2*60;
+      console.log("repos");
+      pause.style.color = 'green';
+      travail.style.color = 'red';
+
+
+    }
+    if (temps <= 0 && !tempsTravail) {
+      tempsTravail = true;
+      temps = 0.5*60;
+      console.log("travail");
+      travail.style.color = 'green';
+      pause.style.color = 'red';
     }
   
   }, 1000)
@@ -30,56 +46,18 @@ function repos(){
 
 }
 
+      const bouton = document.getElementById("bouton");
+      let chronoLancer = false;
+      bouton.addEventListener('click', () => {
 
+  if (!chronoLancer) {
+    lancer();
+    chronoLancer = true;
+    bouton.innerText = "recommencer";
+  }
+  else {
+    location.reload();
+    chronoLancer = false;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-function commencer() {
-  const travail = 0.1;
-  let temps = travail * 60
-
-  setInterval(() => {
-   
-    let minutes = parseInt(temps / 60, 10)
-    let secondes = parseInt(temps % 60, 10)
-  
-    minutes = minutes < 10 ? "0" + minutes : minutes
-    secondes = secondes < 10 ? "0" + secondes : secondes
-  
-    timerElement.innerText = `${minutes}:${secondes}`
-    temps = temps <= 0 ? 0 : temps - 1
-
-    if(minutes == 0 && secondes == 0){
-      console.log("finish");
-      repos();
-    }
-  
-  }, 1000)
-  
-  
-}
-
-
- 
-
-
-const bouton = document.querySelectorAll("button");
-    for(let i = 0; i < bouton.length; i++){
-      bouton[i].addEventListener('click', () => {
-      console.log("hello world");
-      commencer(0.1);
-     
-
-
-      })}
+  }
+})
